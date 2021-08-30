fa75d8575ee045e2e607e26400845dd11f8f895e
Jamie Chin <xjchin@student.unimelb.edu.au>
2021-08-30 15:07:04 +1000

    Aligned and justified based on screen size

--- src/theme.js
+++ src/theme.js
@@ -18,7 +18,7 @@ const Theme = createTheme({
   typography: {
     h1: {
       fontFamily: 'Reem Kufi',
-      fontSize: '5rem',
+      fontSize: '7rem',
     },
     h2: {
       fontFamily: 'Roboto',
--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,38 +1,43 @@
 
 import Theme from "../theme";
 import { ThemeProvider } from "@material-ui/styles";
-import { Typography, Button } from "@material-ui/core";
+import { Typography, Button, Grid } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
+
+
 
 const LandingPage = () => {
   // const url = "http://localhost:3000/LandingPage";
 
   return (
+    
     <ThemeProvider theme={Theme}>
-      <Box mt="230px" mb="140px">
-        <Typography variant="h1" align="center" color="primary">
-          My Daily
-        </Typography>
-        <Typography variant="h1" align="center" color="primary">
-          Planner
-        </Typography>
-      </Box>
+      <Grid container direction="column" alignItems="center" justifyContent="center" style={{ minHeight: "90vh" }}>
+      
+        <Box>
+          <Typography variant="h1" align="center" color="primary">
+            My Daily
+          </Typography>
+          <Typography variant="h1" align="center" color="primary">
+            Planner
+          </Typography>
+        </Box>
 
-      <Box mt="20px">
-        <Button size="medium" variant="contained" color="primary" type="submit">
-          <Typography variant="h2">Log In</Typography>
-        </Button>
-      </Box>
+        <Box mt="20px">
+          <Button size="medium" variant="contained" color="primary" type="submit">
+            <Typography variant="h2">Log In</Typography>
+          </Button>
+        </Box>
 
-      <Box mt="20px">
-        <Button size="medium" variant="contained" color="secondary" type="submit">
-          <Typography variant="h2">Sign Up</Typography>
-        </Button>
-      </Box>
-        
-
-
+        <Box mt="20px">
+          <Button size="medium" variant="contained" color="secondary" type="submit">
+            <Typography variant="h2">Sign Up</Typography>
+          </Button>
+        </Box>
+      
+      </Grid>
     </ThemeProvider>
+    
 )};
 
 export default LandingPage;
