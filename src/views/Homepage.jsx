c59aba1611c9d90be1be8a54531241cb48aaea6f
Jamie Chin <xjchin@student.unimelb.edu.au>
2021-09-03 14:32:15 +1000

    Responsive to phone size

--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -1,6 +1,6 @@
 import Theme from "../themes/landingTheme";
 import { ThemeProvider } from "@material-ui/styles";
-import { Typography, Button } from "@material-ui/core";
+import { Typography, Button,Grid } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Divider from '@material-ui/core/Divider';
@@ -9,50 +9,46 @@ const HomePage = () => {
     return (
         <ThemeProvider theme={Theme}>
             <CssBaseline />
-            <Box mt="230px" mb="140px" bgcolor="primary">
-                <Typography variant="h1" align="center" color="primary">
-                    My Daily
-                </Typography>
-                <Typography variant="h1" align="center" color="primary">
-                    Planner
-                </Typography>
-            </Box>
+            <Grid container direction="column" alignItems="center" justifyContent="center" style={{minHeight: "90vh"}}>
+                <Box>
+                    <Typography variant="h1" align="center" color="primary">
+                        My Daily
+                    </Typography>
+                    <Typography variant="h1" align="center" color="primary">
+                        Planner
+                    </Typography>
+                </Box>
 
-            <Box mt="2px" width={.99} bgcolor="primary" height="60px" marginLeft="1%">
-                <Button fullWidth={1} size="large" style={{ height: "98%", justifyContent: "flex-start" }}>
-                    <Typography variant="h2" style={{ color: "white", padding: "15px 0 0 0" }}>People</Typography>
-                </Button>
-                <Divider style={{ background: "white" }} />
-            </Box>
+                <Box mt="2px" width={.99} bgcolor="primary" height="60px" marginLeft="1%">
+                    <Button fullWidth={true} size="large" style={{ height: "98%", justifyContent: "flex-start" }}>
+                        <Typography variant="h2" style={{ color: "white", padding: "15px 0 0 0" }}>People</Typography>
+                    </Button>
+                    <Divider style={{ background: "white" }} />
+                </Box>
 
-            <Box mt="2px" width={.99} bgcolor="primary" height="60px" marginLeft="1%">
-                <Button fullWidth={1} size="large" style={{ height: "98%", justifyContent: "flex-start" }}>
-                    <Typography variant="h2" style={{ color: "white", padding: "15px 0 0 0" }}>Meetings</Typography>
-                </Button>
-                <Divider style={{ background: "white" }} />
-            </Box>
+                <Box mt="2px" width={.99} bgcolor="primary" height="60px" marginLeft="1%">
+                    <Button fullWidth={true} size="large" style={{ height: "98%", justifyContent: "flex-start" }}>
+                        <Typography variant="h2" style={{ color: "white", padding: "15px 0 0 0" }}>Meetings</Typography>
+                    </Button>
+                    <Divider style={{ background: "white" }} />
+                </Box>
 
-            <Box mt="2px" width={.99} bgcolor="primary" height="60px" marginLeft="1%">
-                <Button fullWidth={1} size="large" style={{ height: "98%", justifyContent: "flex-start" }}>
-                    <Typography variant="h2" style={{ color: "white", padding: "15px 0 0 0" }}>Reminders</Typography>
-                </Button>
-                <Divider style={{ background: "white" }} />
-            </Box>
+                <Box mt="2px" width={.99} bgcolor="primary" height="60px" marginLeft="1%">
+                    <Button fullWidth={true} size="large" style={{ height: "98%", justifyContent: "flex-start" }}>
+                        <Typography variant="h2" style={{ color: "white", padding: "15px 0 0 0" }}>Reminders</Typography>
+                    </Button>
+                    <Divider style={{ background: "white" }} />
+                </Box>
 
-            <Box mt="2px" width={.99} bgcolor="primary" height="60px" position="absolute" bottom="0">
-                <Button size="large" style={{ height: "100%", justifyContent: "flex-start", width: "50%" }}>
-                    <Typography variant="h6" style={{ color: "white" }}>Settings</Typography>
-                </Button>
-                <Button size="large" style={{ height: "100%", justifyContent: "flex-end", width: "50%" }}>
-                    <Typography variant="h6" style={{ color: "white" }}>Logout</Typography>
-                </Button>
-            </Box>
-
-
-
-
-
-
+                <Box mt="2px" width={.99} bgcolor="primary" height="60px" position="fixed" bottom="0">
+                    <Button size="large" style={{ height: "100%", justifyContent: "flex-start", width: "50%" }}>
+                        <Typography variant="h6" style={{ color: "white" }}>Settings</Typography>
+                    </Button>
+                    <Button size="large" style={{ height: "100%", justifyContent: "flex-end", width: "50%" }}>
+                        <Typography variant="h6" style={{ color: "white" }}>Logout</Typography>
+                    </Button>
+                </Box>
+            </Grid>
         </ThemeProvider>
     )
 };
