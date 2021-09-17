04ec926f46d64453d86dad86b98fcb8f7d63afd6
Cas2a <87962538+Cas2a@users.noreply.github.com>
2021-09-17 14:12:55 +1000

    fixed margin issue
    margin issue causing scroll bar to appear was fixed

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -4,7 +4,7 @@ import Theme from "../themes/landingTheme";
 import PageTitle from "../components/PageTitle"
 
 import { ThemeProvider } from "@material-ui/styles";
-import { Typography, Button,Grid } from "@material-ui/core";
+import { Typography, Button, Grid } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Divider from '@material-ui/core/Divider';
@@ -19,7 +19,6 @@ const useStyles = makeStyles({
         justifyContent: "flex-start",
         color: palette.secondary.main,
         padding: "30px 0 10px 2px",
-        marginLeft: "1%",
         marginTop: "2px",
     },
     divider: {
@@ -42,7 +41,7 @@ const useStyles = makeStyles({
 
 
 // the main buttons on the homepage (e.g. People, Meetings)
-const HomePageButton = ({name, linkedComponent}) => {
+const HomePageButton = ({ name, linkedComponent }) => {
     const classes = useStyles();
     return (
         <Box marginLeft="1%">
@@ -55,7 +54,7 @@ const HomePageButton = ({name, linkedComponent}) => {
 }
 
 // buttons to navigate to various site options (e.g. Settings, Logout)
-const OptionsButton = ({name, position}) => {
+const OptionsButton = ({ name, position }) => {
     // interpret position of the text
     let justifyFlex = "flex-start"
     if (position === "right") {
@@ -64,7 +63,7 @@ const OptionsButton = ({name, position}) => {
 
     const classes = useStyles();
     return (
-        <Button className={classes.optionsButton} style={{justifyContent: justifyFlex}}>
+        <Button className={classes.optionsButton} style={{ justifyContent: justifyFlex }}>
             <Typography variant="h5">{name}</Typography>
         </Button>
     )
@@ -86,20 +85,20 @@ const HomePage = () => {
     return (
         <ThemeProvider theme={Theme}>
             <CssBaseline />
-            
+
             <Grid container direction="column" justifyContent="center" style={{ minHeight: "90vh" }}>
-      
-            <PageTitle />
 
-            <Link to="/People" style={{ textDecoration: 'none' }}>
-                <HomePageButton name="People"/>
-            </Link>
+                <PageTitle />
 
-            <Link to="/Meetings" style={{ textDecoration: 'none' }}>
-                <HomePageButton name="Meetings"/>
-            </Link>
+                <Link to="/People" style={{ textDecoration: 'none' }}>
+                    <HomePageButton name="People" />
+                </Link>
 
-            <HomePageButton name="Reminders" />
+                <Link to="/Meetings" style={{ textDecoration: 'none' }}>
+                    <HomePageButton name="Meetings" />
+                </Link>
+
+                <HomePageButton name="Reminders" />
             </Grid>
 
             <OptionsBar />
