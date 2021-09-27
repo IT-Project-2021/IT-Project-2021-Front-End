21e36a0c3675a9a9fc91dbab645274f6433b4859
Jamie Chin <xjchin@student.unimelb.edu.au>
2021-09-27 15:08:37 +1000

    Profile page refactor

--- src/views/MeetingsPage.jsx
+++ src/views/MeetingsPage.jsx
@@ -5,7 +5,6 @@ import Box from "@material-ui/core/Box";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
-import { Link } from "react-router-dom";
 import AddButton from "@material-ui/icons/Add"
 import IconButton from '@material-ui/core/IconButton';
 
--- src/views/ProfilePage.jsx
+++ src/views/ProfilePage.jsx
@@ -4,37 +4,66 @@ import { Typography, Button, TextField, Grid } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Link from '@material-ui/core/Link';
-
-//general components in use
+import { makeStyles } from '@material-ui/core/styles';
 import PageAppBar from "../components/PageAppBar"
 
-const ProfilePage = () => {
+const palette = Theme.palette
+const useStyles = makeStyles({
+    title: {
+      color: palette.primary.main,
+      margin: "20% 20% 100px",
+    },
+    update: {
+        color: palette.primary.main,
+        margin: "10% 10% 100px",
+        width: "50%",
+        minWidth: "400px"
+    },
+    changePasswordButton: {
+        color: palette.primary.main,
+        margin: "50% 10% 100px",
+    },
+    delete: {
+        color: palette.primary.main,
+        margin: "10px 10% 100px",
+    },
+    form: {
+        color: palette.primary.main,
+        margin: "10% 0px 0px 0px",
+        width: "30%",
+        minWidth: "400px",
+    }
+});
 
+
+
+const ProfilePage = () => {
+    const classes = useStyles();
     return (
         <ThemeProvider theme={Theme}>
             <CssBaseline />
-
             <PageAppBar prevPage="/" tab="Profile" />
 
             <Grid container direction="column" alignItems="center" justifyContent="center" >
-                <Box marginTop="clamp(50px, 20%, 100px)">
+                <Box className={classes.title} >
                     <Typography variant="h2">
                         Profile
                     </Typography>
                 </Box>
+
                 <form>
-                    <Box marginTop="clamp(40px, 18%, 70px)" width="30%" minWidth="400px">
+                    <Box className={classes.form}>
                         <TextField label="First Name" placeholder="John" variant="filled" fullWidth />
                     </Box>
 
-                    <Box marginTop="clamp(40px, 18%, 70px)" width="30%" minWidth="400px">
+                    <Box className={classes.form}>
                         <TextField label="Last Name" placeholder="Doe" type="password" variant="filled" fullWidth />
                     </Box>
                     <br />
 
                 </form>
 
-                <Box px="20px" marginTop="clamp(25px, 12%, 50px)">
+                <Box className={classes.update}>
                     <Link to="/HomePage" style={{ textDecoration: 'none' }}>
                         <Button size="medium" type="submit" color="secondary" variant="outlined" style={{ border: '2px solid' }}>
                             <Typography variant="button" color="secondary">Update</Typography>
@@ -42,7 +71,7 @@ const ProfilePage = () => {
                     </Link>
                 </Box>
 
-                <Box px="20px" marginTop="clamp(80px, 25%, 140px)">
+                <Box className={classes.changePasswordButton}>
                     <Link to="/HomePage" style={{ textDecoration: 'none' }}>
                         <Button size="medium" type="submit" variant="contained" color="tertiary">
                             <Typography variant="button" style={{ color: 'black' }}>Change password</Typography>
@@ -51,7 +80,7 @@ const ProfilePage = () => {
                 </Box>
 
 
-                <Box marginTop="clamp(10px, 3%, 18px)" >
+                <Box className={classes.delete}>
                     <Link href="#" style={{ fontSize: "25px", color: '#0353A4' }}>
                         Delete Account
                     </Link>
