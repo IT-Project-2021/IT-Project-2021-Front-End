05b3d48c1d570fccf9f285fd69e15e4c174ceb55
Jamie Chin <xjchin@student.unimelb.edu.au>
2021-10-01 15:46:41 +1000

    Responsive ProfilePage

--- src/views/ProfilePage.jsx
+++ src/views/ProfilePage.jsx
@@ -10,26 +10,24 @@ import PageAppBar from "../components/PageAppBar"
 const palette = Theme.palette
 const useStyles = makeStyles({
     title: {
-      color: palette.primary.main,
-      margin: "7vh 0vh 10vh",
+      color: palette.secondary.main,
+      margin: "7vh 0vh 7vh",
     },
     form: {
         color: palette.primary.main,
         margin: "3vh 0vh 0vh 0vh",
-        width: "10vh",
-        minWidth: "400px",
     },
     update: {
         color: palette.primary.main,
-        margin: "10vh 0vh 3vh"
+        margin: "3vh 0vh 7vh"
     },
     changePasswordButton: {
         color: palette.primary.main,
         margin: "1vh 0vh 0vh",
     },
     delete: {
-        color: palette.primary.main,
-        margin: "0vh 0vh 1vh"
+        color: palette.secondary.main,
+        margin: "1vh 0vh 1vh"
     },
     
 });
@@ -43,7 +41,7 @@ const ProfilePage = () => {
             <CssBaseline />
             <PageAppBar prevPage="/HomePage" tab="Profile" />
 
-            <Grid container direction="column" style={{ height: "90vh" }}>
+            <Grid container direction="column" justifyContent="center" style={{ height: "90vh" }}>
                 <Box className={classes.title} >
                     <Typography variant="h2">
                         Profile
@@ -52,32 +50,29 @@ const ProfilePage = () => {
 
                 <form>
                     <Box className={classes.form}>
-                        <TextField label="First Name" placeholder="John" variant="filled" fullWidth />
+                        <TextField label="First Name" placeholder="John" variant="filled" />
                     </Box>
-
                     <Box className={classes.form}>
-                        <TextField label="Last Name" placeholder="Doe" type="password" variant="filled" fullWidth />
+                        <TextField label="Last Name" placeholder="Doe" type="password" variant="filled" />
                     </Box>
-                    <br />
-
                 </form>
 
                 <Box className={classes.update}>
                     <Button size="medium" type="submit" color="secondary" variant="outlined" style={{ border: '2px solid' }}>
-                        <Typography variant="button" color="secondary">Update</Typography>
+                        <Typography variant="button">Update</Typography>
                     </Button>
                 </Box>
 
                 <Box className={classes.changePasswordButton}>
                     <Link to="/ChangePassword" style={{ textDecoration: 'none' }}>
-                        <Button size="medium" variant="outlined" color="secondary">
+                        <Button size="small" variant="outlined" color="secondary">
                             <Typography variant="button"> Change password </Typography>
                         </Button>
                     </Link>
                 </Box>
 
                 <Box className={classes.delete}>
-                    <Link style={{ textDecoration: 'none', fontSize: "20px", color: '#0353A4' }}>
+                    <Link style={{ textDecoration: 'none', color: '#0353A4' }}>
                         Delete Account
                     </Link>
                 </Box>
