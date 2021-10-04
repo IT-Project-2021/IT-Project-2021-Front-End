9aab556fb59a63eb5c26461baaf6febc38f61abb
Isobel <ibyars@student.unimelb.edu.au>
2021-10-04 11:43:57 +0800

    Resolve merge conflicts

--- src/views/MeetingsPage.jsx
+++ src/views/MeetingsPage.jsx
@@ -7,7 +7,8 @@ import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
 import AddButton from "@material-ui/icons/Add"
 import IconButton from '@material-ui/core/IconButton';
-import PageAppBar from "../components/PageAppBar"
+import PageAppBar from "../components/PageAppBar";
+import { Link } from "react-router-dom";
 
 const palette = Theme.palette
 const useStyles = makeStyles({
