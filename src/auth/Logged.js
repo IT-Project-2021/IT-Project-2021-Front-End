4fd4c224756946f5499320145c714ea24db0cff7
Cas2a <87962538+Cas2a@users.noreply.github.com>
2021-10-16 17:05:02 +1100

    adding tokens to front end

---
+++ src/auth/Logged.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import React, { createContext, useEffect, useState } from "react";
+
+const Logged = createContext();
+
+function LogStatus(props) {
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    async function getLoggedIn() {
+        // const loggedInRes = await axios.get("http://localhost:5000/auth/loggedIn");
+        const loggedInRes = await axios.get(
+            "path to auth middleware"
+        );
+        setLoggedIn(loggedInRes.data);
+    }
+
+    useEffect(() => {
+        getLoggedIn();
+    }, []);
+
+    return (
+        <Logged.Provider value={{ loggedIn, getLoggedIn }}>
+            {props.children}
+        </Logged.Provider>
+    );
+}
+
+export default Logged;
+export { LogStatus };
--- src/index.js
+++ src/index.js
@@ -18,31 +18,36 @@ import ForgotPasswordPage from "./views/ForgotPasswordPage"
 import ProfilePage from "./views/ProfilePage"
 import ChangePasswordPage from "./views/ChangePasswordPage"
 import NewPersonPage from "./views/NewPersonPage"
+import { LogStatus } from "./auth/Logged";
+import axios from 'axios';
 
 
 
 var hist = createBrowserHistory();
+axios.defaults.withCredentials = true;
 
 ReactDOM.render(
-  <Router history={hist}>
-    <ThemeProvider theme={Theme}>
-      <Box align="center" height="100%">
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/PeopleInformation/:id" component={PeopleInfoPage} />
-        <Route path="/HomePage" component={HomePage} />
-        <Route path="/People" component={PeopleListPage} />
-        <Route path="/Login" component={LoginPage} />
-        <Route path="/Meetings" component={MeetingsPage} />
-        <Route path="/Signup" component={SignupPage} />
-        <Route path="/ForgotPassword" component={ForgotPasswordPage} />
-        <Route path="/Profile" component={ProfilePage} />
-        <Route path="/ChangePassword" component={ChangePasswordPage} />
-        <Route path="/AddNewPerson" component={NewPersonPage} />
+  <LogStatus>
+    <Router history={hist}>
+      <ThemeProvider theme={Theme}>
+        <Box align="center" height="100%">
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/PeopleInformation/:id" component={PeopleInfoPage} />
+          <Route path="/HomePage" component={HomePage} />
+          <Route path="/People" component={PeopleListPage} />
+          <Route path="/Login" component={LoginPage} />
+          <Route path="/Meetings" component={MeetingsPage} />
+          <Route path="/Signup" component={SignupPage} />
+          <Route path="/ForgotPassword" component={ForgotPasswordPage} />
+          <Route path="/Profile" component={ProfilePage} />
+          <Route path="/ChangePassword" component={ChangePasswordPage} />
+          <Route path="/AddNewPerson" component={NewPersonPage} />
 
-      </Box>
-    </ThemeProvider>
-  </Router>,
-  document.getElementById('root')
+        </Box>
+      </ThemeProvider>
+    </Router>,
+    document.getElementById('root')
+  </LogStatus>
 
 );
 
