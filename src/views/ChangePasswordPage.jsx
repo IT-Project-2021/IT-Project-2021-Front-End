4e4626a8dcbb8c2bc71b747f10ed0b9d083b7692
Isobel <ibyars@student.unimelb.edu.au>
2021-11-08 16:22:24 +0800

    Passwords can now be changed

--- src/views/ChangePasswordPage.jsx
+++ src/views/ChangePasswordPage.jsx
@@ -6,8 +6,11 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { Link } from "react-router-dom";
 import PageAppBar from "../components/PageAppBar"
 import { makeStyles } from '@material-ui/core/styles';
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import userService from "../services/users";
+import authService from "../services/auth";
+import Cookies from 'universal-cookie'
 
 const palette = Theme.palette
 const useStyles = makeStyles({
@@ -37,26 +40,72 @@ const ChangePasswordPage = () => {
     const [newPass, setNewPass] = useState("");
     const [confirm, setConfirm] = useState("");
     const [passwordError, setPassError] = useState(false);
+    const [userInfo, setUserInfo] = useState({})
+
+    // Get current user info
+    useEffect(() => {
+        const cookies = new Cookies()
+        userService
+            .getInfo(cookies.get("token"))
+            .then(response => {
+                setUserInfo(response.data)
+                console.log("information:", response.data)
+            })
+            .catch(error => {
+                // 401 error occurs if token is either missing or bad
+                if (error.response && error.response.status && (error.response.status === 401)) {
+                    if (cookies.get("token")) {
+                        // The token is invalid
+                        cookies.remove("token", { path: '/' }) 
+                    }
+                    window.location.href = "/login"
+                }
+            })
+    }, [])
 
     async function handleSubmit(e) {
         e.preventDefault();
         setPassError(false);
         if (newPass !== confirm) {
             setPassError(true);
+            alert("Passwords do not match. Please try again!")
         }
         else {
-            try {
-                const data = {
-                    password_hash: newPass,
-                };
-
-                await axios.put("/api/users/:userId", data, {
-                    withCredentials: true,
-                })
-
-            } catch (err) {
-                console.error(err);
+            const newData = {
+                password: newPass,
+            };
+            const oldData = {
+                password: current,
+                email: userInfo.email
             }
+            const cookies = new Cookies()
+            // attempt authentication to confirm that cur pass is correct
+            authService
+                .attemptLogin(oldData)
+                .then(response => {
+                    console.log("Auth attempt succeeded:", response.data)
+                    // Now that cur pass is confirmed, update password
+                    userService
+                        .updateUser(userInfo._id, newData, cookies.get("token"))
+                        .then(response => {
+                            console.log("Updated user:", response.data)
+                            window.location.href = "/Profile"
+                        })
+                        .catch(error => {
+                            // 401 error occurs if token is either missing or bad
+                            if (error.response && error.response.status && (error.response.status === 401)) {
+                                if (cookies.get("token")) {
+                                    // The token is invalid
+                                    cookies.remove("token", { path: '/' }) 
+                                }
+                                window.location.href = "/login"
+                            }
+                        })
+                })
+                .catch(error => {
+                    console.log("Auth failed:", error.response)
+                    alert("Current password is incorrect. Please try again!")
+                })
         }
     }
 
